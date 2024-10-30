9b4eecfedfb6e2be287ce84d60e898e063517122
ch2ohchohch2oh <ch2ohchohch2oh@126.com>
2024-10-30 09:57:39 +0800

    ignore __pycache__ directory and update apiClient to support signer by AKSK

--- utils/api_client.py
+++ utils/api_client.py
@@ -2,14 +2,13 @@
 # Author: Andy Freeman
 # Date: 2024/10/14
 # Description: Keep Hungry Keep Foolish
-
+import json
+import urllib
 import requests
 import logging
-
-from exceptiongroup import catch
-
-from utils import signer
 from utils.load_yaml_content import load_env_config
+from utils.signer import HttpRequest, Signer
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 logger = logging.getLogger()
 
@@ -24,22 +23,71 @@ class ApiClient:
         self.sk = envconfig.get('sk')
 
     def send_request(self, **kwargs):
-        method = kwargs.get('method')
+        method = kwargs.get('method', 'POST')
         url = self.base_url + kwargs.get('url')
+        headers = kwargs.get('headers', {})
+        json_data = kwargs.get('json')
+        data = kwargs.get('data')
+        files = kwargs.get('files')
 
-        sig = signer.Signer()
+        # 创建HttpRequest对象
+        r = HttpRequest(method=method, url=url, headers=headers)
+
+        # 更新HttpRequest对象的body
+        # 处理JSON数据
+        if json_data:
+            if not isinstance(json_data, dict):
+                raise ValueError("JSON data must be a dictionary")
+            r.body = json.dumps(json_data).encode('utf-8')
+            headers['Content-Type'] = 'application/json'
+
+        # 处理表单数据
+        elif data:
+            if isinstance(data, dict):
+                # 表单数据
+                r.body = urllib.parse.urlencode(data).encode('utf-8')
+                headers['Content-Type'] = 'application/x-www-form-urlencoded'
+            elif isinstance(data, str):
+                # json转换字符串
+                r.body = data.encode('utf-8')
+                headers['Content-Type'] = 'application/json'
+
+        # 处理文件内容
+        elif files:
+            if isinstance(files, dict):
+                encoder = MultipartEncoder(fields=files)
+                r.body = encoder
+                headers['Content-Type'] = encoder.content_type
+            elif isinstance(files, str):
+                # 假设files是文件内容
+                r.body = files.encode('utf-8')
+                headers['Content-Type'] = 'application/octet-stream'
+
+        # 更新HttpRequest对象的headers
+        r.headers = headers
+
+        # 签名
+        sig = Signer()
         sig.Key = self.ak
         sig.Secret = self.sk
-        r = signer.HttpRequest(method, url)
-        r.headers = kwargs.get('headers')
-        r.body = kwargs.get('body')
         sig.Sign(r)
 
+
+        # 发送请求
         logger.info(f'ApiClient send_request')
         logger.info(f'send_request url : {url}')
         try:
-            response = requests.request(r.method, r.scheme + "://" + r.host + r.uri, headers=r.headers, data=r.body)
+            if files:
+                response = requests.request(method=method, url=url, headers=r.headers, files=r.body)
+            elif json_data:
+                response = requests.request(method=method, url=url, headers=r.headers, json=json_data)
+            elif data:
+                response = requests.request(method=method, url=url, headers=r.headers, data=r.body)
+            else:
+                response = requests.request(method=method, url=url, headers=r.headers)
+
             return response
+
         except requests.exceptions.HTTPError as http_err:
             logger.info(f'HTTP error occured : {http_err}')
         except requests.exceptions.ConnectionError as conn_err:
@@ -50,4 +98,51 @@ class ApiClient:
             logger.info(f'Something error occured : {req_err}')
         except ValueError as val_err:
             logger.info(f'Error prasing JSON : {val_err}')
+
+
+if __name__ == '__main__':
+    domain_id = '070787c9308010a60f48c00264a347c0'
+
+    user_info = {
+        'user': {
+            'domain_id': domain_id,
+            'name': 'IAMUser3',
+            'password': 'IAMPassword@',
+            'email': 'IAMEmail3@huawei.com',
+            'areacode': '0086',
+            'phone': '12345678913',
+            'enabled': True,
+            'pwd_status': False,
+            'xuser_type': '',
+            'xuser_id': '',
+            'access_mode': 'default',
+            'description': 'IAMDescription'
+        }
+    }
+
+    print(user_info)
+
+    client = ApiClient()
+
+    # response = send_requests(
+    #     method='POST',
+    #     url='/v3.0/OS-USER/users',
+    #     json=user_info
+    # )
+
+
+    response = client.send_request(
+        method='POST',
+        url='/v3.0/OS-USER/users',
+        data=json.dumps(user_info)
+    )
+
+    # response = send_requests(
+    #     method='POST',
+    #     url='https://example.com/api',
+    #     files={'file': ('filename', '文件内容', 'application/octet-stream')}  # 文件内容
+    # )
+
+    print(response.status_code)
+    print(response.content)
 
