109ad84451fc9a3a3f1f3209146f1d9f69758d2b
ch2ohchohch2oh66 <ch2ohchohch2oh66@gmail.com>
2024-11-06 22:16:52 +0800

    更新pytest.ini，设置用例检索机制，以避免TestEnterance.py中test_start()被当作测试用例，导致死循环的问题

---
+++ lets_go.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Author: Andy Freeman
+# Date: 2024/10/11
+# Description: Keep Hungry Keep Foolish
+
+import pytest
+
+
+def lets_go():
+    pytest.main()
+
+
+if '__main__' == __name__:
+    lets_go()
